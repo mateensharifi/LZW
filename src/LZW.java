7f86af660b4dad1dea54105a2c9d713634c31398
mateensharifi <mateensharifi11@gmail.com>
2021-09-08 13:53:39 -0700

    Added BufferedReader

--- src/LZW.java
+++ src/LZW.java
@@ -1,4 +1,15 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class LZW {
-
+	private BufferedReader reader;
+	private HashMap<Integer, Character> dictionary;
+	public LZW(String inputFileName) throws FileNotFoundException {
+		reader = new BufferedReader(new FileReader(inputFileName));
+		dictionary = new HashMap<Integer, Character>();
+	}
+	
 }
