6875acc0f7b3e4906958dd48caaaec4b106240df
mateensharifi <mateensharifi11@gmail.com>
2021-09-08 14:37:05 -0700

    Added parameter to encode

--- src/LZW.java
+++ src/LZW.java
@@ -5,19 +5,21 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class LZW {
-	private BufferedReader reader;
+	//private BufferedReader reader;
 	private HashMap<Integer, String> dictionary;
 	private String first;
 	private String next;
 	public LZW(String inputFileName) throws FileNotFoundException {
-		reader = new BufferedReader(new FileReader(inputFileName));
+		//reader = new BufferedReader(new FileReader(inputFileName));
 		dictionary = new HashMap<Integer, String>();
 	}
-	public String encode () {
-		first = dictionary.get(0); //input character
+	public String encode (String input) {
+		int start = 0;
+		int end = 1;
+		first = input.substring(start, end);
 		int counter = 0;
 		while (counter != dictionary.size() - 1) {
-			next = dictionary.get(counter++); //input character
+			next = input.substring(start + 1, end + 1);
 			if (dictionary.containsValue(first + next)) {
 				first = first + next;
 			}
