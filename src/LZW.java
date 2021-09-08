73f612774991874267455b88abf1926fe060525c
mateensharifi <mateensharifi11@gmail.com>
2021-09-08 14:30:34 -0700

    Drafted encode()

--- src/LZW.java
+++ src/LZW.java
@@ -6,10 +6,28 @@ import java.util.HashMap;
 
 public class LZW {
 	private BufferedReader reader;
-	private HashMap<Integer, Character> dictionary;
+	private HashMap<Integer, String> dictionary;
+	private String first;
+	private String next;
 	public LZW(String inputFileName) throws FileNotFoundException {
 		reader = new BufferedReader(new FileReader(inputFileName));
-		dictionary = new HashMap<Integer, Character>();
+		dictionary = new HashMap<Integer, String>();
 	}
-	
+	public String encode () {
+		first = dictionary.get(0); //input character
+		int counter = 0;
+		while (counter != dictionary.size() - 1) {
+			next = dictionary.get(counter++); //input character
+			if (dictionary.containsValue(first + next)) {
+				first = first + next;
+			}
+			else {
+				return next;
+			}
+			dictionary.put(dictionary.size() - 1, first + next);
+			first = next;
+			counter++;
+		}
+		return first;
+	}
 }
